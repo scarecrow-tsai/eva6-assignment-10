aa3b28daf93156d544871b79939ce9ed49c5e511
Akshaj Verma <akshajverma.oo7@gmail.com>
2021-07-29 00:06:14 +0530

    updated resnet.

--- app/models/resnet.py
+++ app/models/resnet.py
@@ -4,43 +4,24 @@ import torch.nn as nn
 class BaseBlock(nn.Module):
     def __init__(self, c_in, c_out, spatial_downsample):
         super(BaseBlock, self).__init__()
-
         self.c_in = c_in
         self.c_out = c_out
         self.spatial_downsample = spatial_downsample
-        self.relu = nn.ReLU()
 
-        if self.spatial_downsample:
-            self.base_block = nn.Sequential(
-                self.conv_block(
-                    c_in=self.c_in, c_out=self.c_out, kernel_size=3, stride=2, padding=1
-                ),
-                nn.ReLU(),
-                self.conv_block(
-                    c_in=self.c_out,
-                    c_out=self.c_out,
+        if spatial_downsample:
+            self.conv_block_1 = nn.Sequential(
+                nn.Conv2d(
+                    in_channels=self.c_in,
+                    out_channels=self.c_out,
                     kernel_size=3,
-                    stride=1,
+                    stride=2,
                     padding=1,
                 ),
-            )
-        else:
-            self.base_block = nn.Sequential(
-                self.conv_block(
-                    c_in=self.c_in, c_out=self.c_out, kernel_size=3, stride=1, padding=1
-                ),
+                nn.BatchNorm2d(num_features=self.c_out),
                 nn.ReLU(),
-                self.conv_block(
-                    c_in=self.c_out,
-                    c_out=self.c_out,
-                    kernel_size=3,
-                    stride=1,
-                    padding=1,
-                ),
             )
 
-        if self.c_in != self.c_out:
-            self.pointwise_conv = nn.Conv2d(
+            self.pointwise = nn.Conv2d(
                 in_channels=self.c_in,
                 out_channels=self.c_out,
                 kernel_size=1,
@@ -48,115 +29,142 @@ class BaseBlock(nn.Module):
                 padding=0,
             )
 
-    def forward(self, x):
-        identity_layer = x
-        x = self.base_block(x)
-
-        if self.c_in == self.c_out:
-            x += identity_layer
         else:
-            identity_layer = self.pointwise_conv(identity_layer)
-            x += identity_layer
+            self.conv_block_1 = nn.Sequential(
+                nn.Conv2d(
+                    in_channels=self.c_in,
+                    out_channels=self.c_out,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                ),
+                nn.BatchNorm2d(num_features=self.c_out),
+                nn.ReLU(),
+            )
 
+            self.pointwise = nn.Conv2d(
+                in_channels=self.c_in,
+                out_channels=self.c_out,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=self.c_out,
+                out_channels=self.c_out,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.BatchNorm2d(num_features=self.c_out),
+        )
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = x
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+
+        identity = self.pointwise(identity)
+
+        x += identity
         x = self.relu(x)
 
         return x
 
-    def conv_block(self, c_in, c_out, **kwargs):
-        seq_block = nn.Sequential(
-            nn.Conv2d(in_channels=c_in, out_channels=c_out, bias=False, **kwargs),
-            nn.BatchNorm2d(num_features=c_out),
-        )
 
-        return seq_block
-
-
-class BaseLayer(nn.Module):
-    def __init__(self, c_in, c_out, spatial_downsample):
-        super(BaseLayer, self).__init__()
-
-        self.c_in = c_in
-        self.c_out = c_out
-        self.spatial_downsample = spatial_downsample
-
-        self.base_layer = nn.Sequential(
-            BaseBlock(
-                c_in=self.c_in,
-                c_out=self.c_out,
-                spatial_downsample=self.spatial_downsample,
-            ),
-            BaseBlock(c_in=self.c_out, c_out=self.c_out, spatial_downsample=False),
-        )
+class BottleNeckBlock(nn.Module):
+    def __init__(self):
+        super(BottleNeckBlock, self).__init__()
+        pass
 
     def forward(self, x):
-        x = self.base_layer(x)
-
         return x
 
 
 class ResNet(nn.Module):
-    def __init__(self, num_input_channels, num_classes):
+    def __init__(self, input_channels, num_classes, block_cfg):
         super(ResNet, self).__init__()
 
-        self.num_input_channels = num_input_channels
+        self.block_cfg = block_cfg
         self.num_classes = num_classes
+        self.input_channels = input_channels
 
-        self.conv = nn.Sequential(
+        self.block_0 = nn.Sequential(
             nn.Conv2d(
-                in_channels=self.num_input_channels,
-                out_channels=32,
-                kernel_size=3,
+                in_channels=self.input_channels,
+                out_channels=64,
+                kernel_size=5,
                 stride=1,
-                padding=1,
-                bias=False,
+                padding=3,
+                dilation=2,
             ),
-            nn.BatchNorm2d(num_features=32),
+            nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
+            nn.AdaptiveAvgPool2d((56, 56)),
         )
-        self.layer_1 = BaseLayer(c_in=32, c_out=64, spatial_downsample=True)
-        self.layer_2 = BaseLayer(c_in=64, c_out=64, spatial_downsample=False)
-        self.layer_3 = BaseLayer(c_in=64, c_out=64, spatial_downsample=False)
 
-        self.layer_4 = BaseLayer(c_in=64, c_out=128, spatial_downsample=True)
-        self.layer_5 = BaseLayer(c_in=128, c_out=128, spatial_downsample=False)
-        self.layer_6 = BaseLayer(c_in=128, c_out=128, spatial_downsample=False)
+        self.block_1 = self._build_block(
+            c_in=64, c_out=64, num_layers=self.block_cfg[0], is_block_1=True
+        )
+        self.block_2 = self._build_block(
+            c_in=64, c_out=128, num_layers=self.block_cfg[1]
+        )
+        self.block_3 = self._build_block(
+            c_in=128, c_out=256, num_layers=self.block_cfg[2]
+        )
+        self.block_4 = self._build_block(
+            c_in=256, c_out=512, num_layers=self.block_cfg[3]
+        )
 
-        self.layer_7 = BaseLayer(c_in=128, c_out=256, spatial_downsample=True)
-        self.layer_8 = BaseLayer(c_in=256, c_out=256, spatial_downsample=False)
-        self.layer_9 = BaseLayer(c_in=256, c_out=256, spatial_downsample=False)
-
-        self.layer_10 = BaseLayer(c_in=256, c_out=512, spatial_downsample=True)
-        self.layer_11 = BaseLayer(c_in=512, c_out=512, spatial_downsample=False)
-
-        self.gap = nn.AvgPool2d(kernel_size=4)
-
-        self.final_conv = nn.Conv2d(
-            in_channels=512, out_channels=self.num_classes, kernel_size=1, stride=1
+        self.avg_pool = nn.AvgPool2d(kernel_size=7)
+        self.fc = nn.Conv2d(
+            in_channels=512,
+            out_channels=self.num_classes,
+            kernel_size=1,
+            stride=1,
+            padding=0,
         )
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.layer_4(x)
-        x = self.layer_5(x)
-        x = self.layer_6(x)
-        x = self.layer_7(x)
-        x = self.layer_8(x)
-        x = self.layer_9(x)
-        x = self.layer_10(x)
-        x = self.layer_11(x)
+        x = self.block_0(x)
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.block_3(x)
+        x = self.block_4(x)
 
-        x = self.gap(x)
-        x = self.final_conv(x)
+        x = self.avg_pool(x)
+        x = self.fc(x)
 
         return x
+
+    def _build_block(self, c_in, c_out, num_layers, is_block_1=False):
+        if is_block_1:
+            return nn.Sequential(
+                *[
+                    BaseBlock(c_in=c_in, c_out=c_out, spatial_downsample=False)
+                    if i == 0
+                    else BaseBlock(c_in=c_out, c_out=c_out, spatial_downsample=False)
+                    for i, _ in enumerate(range(num_layers))
+                ]
+            )
+        else:
+            return nn.Sequential(
+                *[
+                    BaseBlock(c_in=c_in, c_out=c_out, spatial_downsample=True)
+                    if i == 0
+                    else BaseBlock(c_in=c_out, c_out=c_out, spatial_downsample=False)
+                    for i, _ in enumerate(range(num_layers))
+                ]
+            )
 
 
 if __name__ == "__main__":
     from torchinfo import summary
 
     print("Model Summary: \n")
-    model = ResNet(num_input_channels=3, num_classes=200)
+    model = ResNet(input_channels=3, num_classes=200, block_cfg=[3, 4, 6, 3])
     summary(model, input_size=(2, 3, 64, 64))
